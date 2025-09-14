3940ebdb5abcb0667c8999eb10832f5102e2bcec
jazeyttv <jacejohns19@gmail.com>
2025-09-13 20:54:43 -0400

    Test change for Discord webhook notification

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Cali Roleplay Network Python app is running!'
+    return 'Cali Roleplay Network Python app is running! (test change)'
 
 if __name__ == '__main__':
     app.run(debug=True)
